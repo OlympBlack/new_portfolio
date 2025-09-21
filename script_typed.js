4802a5b846beaefe78fde7a5afdb2914e13a9d37
Jules-christ GBASSI <juleschristgbassi9@gmail.com>
2025-09-21 08:42:39 +0200

    Added typed.js

---
+++ script_typed.js
@@ -0,0 +1,6 @@
+var typed = new Typed('#typed-output', {
+        strings: ["Développeur •", "Designer •", "Monteur audiovisuel"],
+        typeSpeed: 50, 
+        backSpeed: 25, 
+        loop: true
+      });
